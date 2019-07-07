f819c524b1b573cccb714aabd12cc4c4763f233d
MrSalamander <51835611+MrSalamander@users.noreply.github.com>
2019-07-07 12:18:06 -0300

    Jogo (+ MENU, SOM, BACKGROUND).c
    Galo Negro

--- gamecode.c
+++ gamecode.c
@@ -8,6 +8,7 @@ typedef struct Player //Estrutura com parâmetros do player e alguns parâmetros
     int firerate;
     Color cor_nave;
     Color cor_bala;
+    float raio;
 }Player;
 
 typedef struct Bala //Estrutura com parâmetros para movimentação dos tiros
@@ -16,11 +17,137 @@ typedef struct Bala //Estrutura com parâmetros para movimentação dos tiros
     bool ativa;
 }Bala;
 
+/*typedef struct Inimigu//estrutura para fazer os inimigos e futuramente suas balas
+{
+    Vector2 posicao;
+    bool ativa;
+    Color cordoini;
+    float raio;
+}Inimigu*/
+
 static Player jogador = {0};
 static Bala bala[50] = {0}; //O vetor bala é necessário pois cada elemento dele é uma bala na tela
+//static Inimigu foe[50]={0}//O vetor bala é necessário pois cada elemento dele é um na tela
+
+void volume(float* vol){
+    if ((IsKeyPressed(KEY_UP))&&(*vol<1.0f)){
+        *vol+=0.1f;
+    }
+    if ((IsKeyPressed(KEY_DOWN))&&(*vol>0.0f)){
+        *vol-=0.1f;
+    }
+    SetMasterVolume(*vol);
+}
+
+void creditos()
+{   
+    while(1)
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("CREDITOS",150,50,70,RAYWHITE);
+        DrawText("Lucas Fernandes Lins",70,150,50,RAYWHITE);
+        DrawText("Matheus Felipe Lima",70,250,50,RAYWHITE);
+        DrawText("Jose Rodrigues Neto",70,350,50,RAYWHITE);
+        DrawText("Joao Victor Galdino",70,450,50,RAYWHITE);
+        DrawText("Luiz Fernando Barbosa",70,550,50,RAYWHITE);
+        DrawText("Pressione M para sair",100,750,20,RAYWHITE);
+        EndDrawing();
+        if(IsKeyPressed('M'))
+            break;
+        
+    }
+}
 
 
-void Movimento(void) //Função de movimentação
+void menuu (Texture2D* bck,Texture2D* nave,float* vol,Texture2D* fundo,Sound* menu)
+{
+   PlaySound(*menu);   
+   while(1)
+        {
+           
+           volume(vol);
+           int posicaodomousex=GetMouseX();
+           int posicaodomousey=GetMouseY(); 
+            BeginDrawing();
+        
+                    ClearBackground(RAYWHITE);
+                        
+                    //desenhando o fundo
+                    DrawTexture(*bck,0,0,RAYWHITE);
+                    //DrawText(TextFormat("%i %i",posicaodomousex ,posicaodomousey), 190, 200, 20, LIGHTGRAY); 
+                    
+                        if (((posicaodomousex>=415)&&(posicaodomousex<=550))&&(posicaodomousey>=570)&&(posicaodomousey<=585))
+                        {
+                            DrawText("Novo Jogo",400,560,40,LIGHTGRAY);
+                            if(IsMouseButtonDown(0)){
+                                StopSound(*menu);
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            DrawText("Novo Jogo",415,570,25,LIGHTGRAY);
+                        }
+                        if (((posicaodomousex>=415)&&(posicaodomousex<=500))&&(posicaodomousey>=620)&&(posicaodomousey<=635))
+                        {
+                            DrawText("Créditos",400,610,40,LIGHTGRAY);
+                            if(IsMouseButtonDown(0))
+                                creditos();
+                        }
+                        else
+                        {
+                            DrawText("Créditos",415,620,25,LIGHTGRAY);
+                        }
+                        if (((posicaodomousex>=415)&&(posicaodomousex<=460))&&(posicaodomousey>=670)&&(posicaodomousey<=695))
+                        {
+                            DrawText("Sair",400,660,40,LIGHTGRAY);
+                            if(IsMouseButtonDown(0))
+                            {
+                                UnloadTexture(*bck); 
+                                UnloadTexture(*nave);  
+                                UnloadTexture(*fundo); 
+                                UnloadSound(*menu);
+                                CloseWindow();
+                            }
+                                
+                        }
+                        else
+                        {
+                            DrawText("Sair",415,670,25,LIGHTGRAY);
+                        }
+        
+                
+                    
+                EndDrawing();
+        }
+}
+
+
+void Pause(int* pont)
+{   
+   
+        
+        while(1)
+        {
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("PAUSE",150,50,50,RAYWHITE);
+            DrawText("Para voltar pressione P",70,150,20,RAYWHITE);
+            DrawText("Para ir para o menu pressione M",70,250,20,RAYWHITE);
+            EndDrawing();
+            if(IsKeyPressed(KEY_P))
+                break;
+            if(IsKeyPressed('M'))
+            {
+              (*pont) = 1;
+              break;
+            }
+        }
+    
+}
+
+void Movimento() //Função de movimentação
 {
     if(IsKeyDown('W'))
     {
@@ -38,7 +165,9 @@ void Movimento(void) //Função de movimentação
     {
         jogador.nave.x += jogador.vel.x;
     }
+    
 }
+
 
 void Tiro(Player jogador,Bala bala[50],int* firerate)
 {
@@ -52,7 +181,7 @@ void Tiro(Player jogador,Bala bala[50],int* firerate)
                 if(!bala[i].ativa)
                 {
                     //A bala inicia na posição do player, na "ponta da frente"
-                    bala[i].posicao.x = jogador.nave.x; 
+                    bala[i].posicao.x = jogador.nave.x+20; 
                     bala[i].posicao.y = jogador.nave.y - 8;
                     bala[i].ativa = true;
                     break;
@@ -80,9 +209,15 @@ void Tiro(Player jogador,Bala bala[50],int* firerate)
 
 int main(void)
 {
+    //criando variavel para menu ingame
+    int checkmenu=0;
     //Definindo resolução da tela
     const int Largura_Tela = 720;
     const int Altura_Tela = 876;
+    //criando variavel dos arquivos de audio
+    Sound menu;
+    //criando var de volume
+    float vol=1.0f;
     
     //Definindo parâmetros do player(e alguns dos tiros do player)
     jogador.nave.x = 300;
@@ -94,37 +229,68 @@ int main(void)
     jogador.shotspeed.y = 15;
     jogador.cor_nave = BLACK;
     jogador.cor_bala = ORANGE;
+    jogador.raio = 10;
+    //declarando float de movimento da tela
+     float movbackground = 0.0f;
     
-    //Inicializando janela
+    
+    //Inicializando janela e áudio
     InitWindow(Largura_Tela,Altura_Tela,"Starlight Drift");
+    InitAudioDevice();
     
-    float movbackground = 0.0f;
-    
-    Image background = LoadImage("C:/Users/Luiz Fernando/Pictures/space.png");
+    //carregando imagens e criando texturas, favor copiar o caminho e copiar
+    Image imageyy = LoadImage("/raylib/StarlightDrift/texture/fundo.png");
+    Image background = LoadImage("/raylib/StarlightDrift/texture/space.png");
+    Image nave1 = LoadImage("/raylib/StarlightDrift/texture/nave.png");
     ImageResize(&background,720,876);
+    ImageResize(&nave1,40,50);
+    Texture2D bck = LoadTextureFromImage(imageyy);
+    Texture2D nave = LoadTextureFromImage(nave1);
     Texture2D fundo = LoadTextureFromImage(background);
     UnloadImage(background);
+    UnloadImage(nave1);
+    UnloadImage(imageyy);
+    
+    //carregando música na ram
+    menu=LoadSound("/raylib/StarlightDrift/sounds/Main Menu.mp3");
     
     SetTargetFPS(60);
-    while(!WindowShouldClose())
+    
+    while(1)
+    {   
+      //impedindo loop infinito
+        checkmenu=0;
+        //funcao menu
+        
+        menuu(&bck,&nave,&vol,&fundo,&menu);
+        
+    while(1)
     {
         //Começando a desenhar e chamando as funções
-        BeginDrawing();
         Movimento();
-        
-        movbackground += 6.0; //velocidade do background
+        if(IsKeyPressed(KEY_P))
+            {
+                Pause(&checkmenu);
+            }
+        if(checkmenu==1)
+            break;
+        movbackground += 3.0; //velocidade do background
         if(movbackground >= background.height) movbackground = 0; //looping do background
+        
+        BeginDrawing();
         ClearBackground(RAYWHITE);
-        //Muito importante
         DrawTextureEx(fundo,(Vector2){0,movbackground},0.0f,1.0f,WHITE);
         DrawTextureEx(fundo,(Vector2){0,-background.height + movbackground},0.0f,1.0f,WHITE);
-        Tiro(jogador,bala,&jogador.firerate);
+        //Muito importante
         DrawText("homem negro fodase",Largura_Tela/2,Altura_Tela/2,20,LIGHTGRAY);
-        DrawCircle(jogador.nave.x,jogador.nave.y,8,jogador.cor_nave);
+        //DrawCircle(jogador.nave.x,jogador.nave.y,8,jogador.cor_nave);
+        DrawTexture(nave,jogador.nave.x,jogador.nave.y,RAYWHITE);
+        Tiro(jogador,bala,&jogador.firerate);
         
         EndDrawing();
     }
-    UnloadTexture(fundo);
-    
+}
+    UnloadTexture(bck); 
+    UnloadTexture(nave);
     return 0;
 }
